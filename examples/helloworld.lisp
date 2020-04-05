ad1772225d85d4119aa31c1e97f60c4e7e240560
Michael Chen <cwchen123@gmail.com>
2020-04-05 14:25:02 +0800

    Add an example

---
+++ examples/helloworld.lisp
@@ -0,0 +1,11 @@
+(load "common.lisp")
+
+(defun main ()
+  (puts "Hello World")
+  (quit-with-status 0))
+
+(if (equal :windows (platform))
+    (defvar *program* "program.exe")
+    (defvar *program* "program"))
+
+(compile-program *program* #'main)
