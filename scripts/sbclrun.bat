25df3dddd78f785ce2c08c5154d2be831fee423b
Michael Chen <cwchen123@gmail.com>
2020-04-06 14:17:26 +0800

    Update sbclrun.bat

--- scripts/sbclrun.bat
+++ scripts/sbclrun.bat
@@ -1,5 +1,7 @@
 @echo off
-rem The script assumes that SBCL is in system path.
+
+rem Check whether SBCL is available.
+sbcl --version >nul 2>&1 || (echo "No SBCL on the system" && exit 1)
 
 rem Get the path of a Lisp script from first argument.
 set script=%1
@@ -10,4 +12,5 @@ if not exist %script% do echo "Not a valid Lisp script" && exit /b 1
 rem Consume first argument.
 shift
 
+rem Run SBCL in batch mode, setting the external format to UTF-8.
 sbcl --noinform --eval "(setf sb-impl::*default-external-format* :UTF-8)" --script %*
