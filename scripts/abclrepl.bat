d50e0bf417c7ce2aeff533a956ff2e6835276bed
Michael Chen <cwchen123@gmail.com>
2020-04-06 14:21:54 +0800

    Create abclrepl.bat

---
+++ scripts/abclrepl.bat
@@ -0,0 +1,10 @@
+@echo off
+
+rem Check whether Java is available.
+java -version >nul 2>&1 || ( echo "No Java on this system" && exit 1)
+
+rem Get the root path of current batch script.
+set rootdir=%~dp0
+
+rem Run Clozure CL compiler.
+java -jar %rootdir%\abcl.jar -jar %rootdir%\abcl-contrib.jar %*
