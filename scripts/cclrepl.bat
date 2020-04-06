f462d5205e54683e6c8173b6c9343f66b0cdc54c
Michael Chen <cwchen123@gmail.com>
2020-04-06 14:08:01 +0800

    Update cclrepl.bat

--- scripts/cclrepl.bat
+++ scripts/cclrepl.bat
@@ -2,7 +2,7 @@
 rem Place the script to the root of Clozure CL.
 
 rem Set CCL according to hardware archtecture.
-if "AMD64" == %PROCESSOR_ARCHITECTURE% (set ccl=wx86cl64.exe) else (set ccl=wx86cl.exe)
+if "AMD64" == "%PROCESSOR_ARCHITECTURE%" (set ccl=wx86cl64.exe) else (set ccl=wx86cl.exe)
 
 rem Get the root path of current batch script.
 set rootdir=%~dp0
