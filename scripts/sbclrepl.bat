161423533b8474de427cce783c7f7e43167c326a
Michael Chen <cwchen123@gmail.com>
2020-04-06 14:16:09 +0800

    Update sbclrepl.bat

--- scripts/sbclrepl.bat
+++ scripts/sbclrepl.bat
@@ -1,4 +1,7 @@
 @echo off
-rem The script assumes that SBCL is in system path.
 
+rem Check whether SBCL is available.
+sbcl --version >nul 2>&1 || (echo "No SBCL on the system" && exit 1)
+
+rem Run SBCL, setting the external format to UTF-8
 sbcl --eval "(setf sb-impl::*default-external-format* :UTF-8)" %*
