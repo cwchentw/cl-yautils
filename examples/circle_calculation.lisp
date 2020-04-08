214242dead54a4119240e8491048cb375a0a6c2a
Michael Chen <cwchen123@gmail.com>
2020-04-08 19:50:38 +0800

    Create circle_calculation.lisp

---
+++ examples/circle_calculation.lisp
@@ -0,0 +1,19 @@
+(load "common.lisp")
+
+(defun circle-circumference (radius)
+  (when (not (not pi))
+        (mathematical-constant))
+  (* 2 pi radius))
+
+(defun circle-area (radius)
+   (when (not (not pi))
+         (mathematical-constant))
+   (* pi (expt radius 2)))
+
+(defun main ()
+   (prog ((radius 10))
+     (puts (circle-circumference radius))
+     (puts (circle-area radius))
+     (quit-with-status 0)))
+
+(main)
