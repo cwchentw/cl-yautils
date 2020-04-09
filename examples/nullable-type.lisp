acf914ce3935454e3df9f34d2cca3119c41edf11
Michael Chen <cwchen.tw@outlook.com>
2020-04-09 11:24:47 +0800

    Update an example

--- examples/nullable-type.lisp
+++ examples/nullable-type.lisp
@@ -2,13 +2,16 @@
 
 (use-package 'cl-yautils)
 
+(defun integer-or-null-p (obj)
+  (typep obj '(nullable integer)))
+
 (defun main ()
   ; 3 is either integer or null.
-  (assert (typep 3 '(nullable integer)))
+  (assert (integer-or-null-p 3))
   ; nil is either integer or null.
-  (assert (typep nil '(nullable integer)))
+  (assert (integer-or-null-p nil))
   ; "3" is neither integer nor null.
-  (assert (equal nil (typep "3" '(nullable integer))))
+  (assert (equal nil (integer-or-null-p "3")))
   (quit-with-status))
 
 (main)
