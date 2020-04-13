7c4b01747427a3025fd55f654b31f86ad1c42572
Michael Chen <cwchen.tw@outlook.com>
2020-04-13 19:30:40 +0800

    Update args.lisp

--- examples/args.lisp
+++ examples/args.lisp
@@ -10,6 +10,14 @@
           #+ecl    (args (rest (rest (rest args))))
           ; In ABCL, no loading script in argument(s).
          )
+    ; Print out unprocessed arguments.
+    (write-line "Unprocessed argument(s):")
+    (puts (argument-vector))
+    
+    (write-line "")  ; Separator.
+
+    ; Print out processed arguments.
+    (write-line "Processed argument(s):")
     (puts args))
   (finish-output)
   (quit-with-status))
