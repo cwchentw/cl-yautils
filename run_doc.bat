5bbde3fc4d715d426c267d0d11d7139880b12b83
Michael Chen <cwchen.tw@outlook.com>
2020-04-21 22:09:27 +0800

    Update documentation-related batch scripts

--- run_doc.bat
+++ run_doc.bat
@@ -1,6 +1,10 @@
+@echo off
+
+rem Check whether Python is available.
 python --version >nul 2>&1 || (
-    echo "No Python on the system"
+    echo No Python on the system 1>&2
     exit /B 1
 )
 
+rem Run a development web server on the Codex output path.
 python -m http.server --directory docs\build\cl-yautils\html 8080
