a872a83ef03a3cc7b92f7ae286a4ed9c5e2e8224
Michael Chen <cwchen.tw@outlook.com>
2020-04-26 09:35:16 +0800

    Remake the wrapper for SBCL on Windows

--- scripts/sbclrepl.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-rem Check whether SBCL is available.
-sbcl --version >nul 2>&1 || (echo "No SBCL on the system" && exit 1)
-
-rem Run SBCL, setting the external format to UTF-8
-sbcl --eval "(setf sb-impl::*default-external-format* :UTF-8)" %*
--- scripts/sbclrun.bat
+++ scripts/sbclrun.bat
@@ -1,16 +1,27 @@
 @echo off
 
 rem Check whether SBCL is available.
-sbcl --version >nul 2>&1 || (echo "No SBCL on the system" && exit 1)
+sbcl --version >nul 2>&1 || (
+    echo No SBCL on the system >&2
+    exit /B 1
+)
 
 rem Get the path of a Lisp script from first argument.
 set script=%1
 
 rem Check whether the Lisp script is valid.
-if not exist %script% do echo "Not a valid Lisp script" && exit /b 1
+rem Run in batch mode if %script% is a file.
+if exist %script% goto batch_mode
 
-rem Consume first argument.
-shift
+rem Fallback to interactive mode.
+goto interactive_mode
 
-rem Run SBCL in batch mode, setting the external format to UTF-8.
-sbcl --noinform --eval "(setf sb-impl::*default-external-format* :UTF-8)" --load %*
+:batch_mode
+rem %script% is the first argument. Hence, there is no need to shift first argument.
+sbcl --noinform --eval "(setf sb-impl::*default-external-format* :UTF-8)" --script %*
+
+rem Exit the program with inherited return value.
+exit /B %ERRORLEVEL%
+
+:interactive_mode
+sbcl %*
