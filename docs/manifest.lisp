37ec41d3314e0361bfffedde440d22d4b32cfa90
Michelle Chen <cwchen.tw@outlook.com>
2022-02-24 05:19:54 +0800

    Update some text

--- docs/manifest.lisp
+++ docs/manifest.lisp
@@ -1,7 +1,7 @@
 (:docstring-markup-format :scriba
  :systems (:cl-yautils)
  :documents ((:title "cl-yautils"
-              :authors ("Michael Chen")
+              :authors ("Michelle Chen")
               :output-format (:type :multi-html
                              :template :minima)
               :sources ("manual.scr"))))
